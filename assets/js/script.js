98280bb77c112b88f945591d1531a5ecd631d5e3
naz-coder <favouritenaz@yahoo.com>
2020-09-02 23:48:19 -0500

    js added

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+(function($){
+    $.Shop=function(element){
+        this.$element=$(element);//top-level element
+        this.init();
+    };
+    $.Shop.prototype={
+        init:function(){
+            //initializes properties and methods
+        }
+    };
+    $(function(){
+        var shop=new$Shop("#site");//object's instance
+    });
+})(jQuery);
